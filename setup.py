a8eb29baae86994893fad2445ba449285d6fbadb
Quantum <quantum2048@gmail.com>
2020-10-03 01:56:47 -0400

    Release 0.0.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as f:
 
 setup(
     name='win2xcur',
-    version='0.0.2',
+    version='0.0.3',
     packages=find_packages(),
     install_requires=['Wand'],
 
