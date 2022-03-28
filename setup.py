b5308cbb1e2a891661bf27e4e7766362e397c7a5
Quantum <quantum2048@gmail.com>
2022-03-28 00:50:04 -0400

    Release version 0.1.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as f:
 
 setup(
     name='win2xcur',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=['numpy', 'Wand'],
 
