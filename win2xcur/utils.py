f9358ed9d151c8912a87754077495f61420b203e
Tech-Tac <im.tech.tac@gmail.com>
2026-06-13 02:20:14 +0300

    Unpremultiply alpha when reading xcursor

--- win2xcur/utils.py
+++ win2xcur/utils.py
@@ -10,3 +10,12 @@ def premultiply_alpha(source: bytes) -> bytes:
     buffer[1::4] *= alpha
     buffer[2::4] *= alpha
     return buffer.astype(np.uint8).tobytes()
+
+def unpremultiply_alpha(source: bytes) -> bytes:
+    buffer: np.ndarray[Any, np.dtype[np.double]] = np.frombuffer(source, dtype=np.uint8).astype(np.double)
+    alpha = buffer[3::4] / 255.0
+    np.divide(buffer[0::4], alpha, out=buffer[0::4], where=alpha > 0)
+    np.divide(buffer[1::4], alpha, out=buffer[1::4], where=alpha > 0)
+    np.divide(buffer[2::4], alpha, out=buffer[2::4], where=alpha > 0)
+    buffer = np.clip(buffer, 0, 255)
+    return buffer.astype(np.uint8).tobytes()
